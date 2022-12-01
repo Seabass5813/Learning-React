e03490b1f034862c5757e8354d1da27951366f88
Sebastian Ortiz-Cabello <sebastian.ortiz@jockey.com>
2022-12-01 15:21:00 -0600

    Chapter 8 - Using State (useState Hook)

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,27 @@
+import { useState } from "react";
+
 const Home = () => {
+    
+    //let name = "mario"
+
+    const [name, setName] = useState('mario');
+    const [age, setAge] = useState(25);
+
 
     const handleClick = () => {
-        console.log('hello peeps')
+        /*name = "luigi";
+        console.log(name)*/
+        setName('luigi')
+        setAge(30);
     }
 
-    const handleClickAgain = (name) => {
-        console.log('hello ' + name)
-    }
+
 
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <p> { name } is { age } years old</p>
             <button onClick={handleClick}>Click ME</button>
-            <button onClick={() => handleClickAgain("John")}>Click Me 2</button>
         </div>
      );
 }
