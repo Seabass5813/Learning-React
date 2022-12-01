523044582b7ae9c9cc98541356aa5a3d6e366acf
Sebastian Ortiz-Cabello <sebastian.ortiz@jockey.com>
2022-12-01 16:03:36 -0600

    Chapter 9 - Intro to React Dev Tools (Watch Video)

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navbar from './Navbar';
 import Home from './Home';
 
 function App() {
-  const title = 'Welcome to the New Blog';
+  /*const title = 'Welcome to the New Blog';*/
   const likes = 50
 
   //booleans and objects will not display on websites. Like the one below.
