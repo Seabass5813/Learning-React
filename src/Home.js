1733dde829c2fc951694d4e0d9f0c159b8ceaedc
Sebastian Ortiz-Cabello <sebastian.ortiz@jockey.com>
2022-12-02 11:06:32 -0600

    Chapter 12 - Reusing Components

--- src/Home.js
+++ src/Home.js
@@ -7,10 +7,11 @@ const Home = () => {
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ])
-    
+
     return ( 
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs"/>
+            <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's blogs" />
         </div>
      );
 }
