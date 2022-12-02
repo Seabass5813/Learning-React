6d6720ac42daad953b5c7d4f96fc7c6010645ad2
Sebastian Ortiz-Cabello <sebastian.ortiz@jockey.com>
2022-12-02 13:30:16 -0600

    Chapter 15 - useEffect Dependencies

--- src/Home.js
+++ src/Home.js
@@ -8,6 +8,8 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ])
 
+    const [name, setName] = useState('mario');
+
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
@@ -16,11 +18,14 @@ const Home = () => {
     //The hook below is used every time the website renders something.
     useEffect(() => {
         console.log('use effect ran')
-    });
+        console.log(name);
+    }, [name]);
 
     return ( 
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
+            <button onClick={() => setName('luigi')}>change name</button>
+            <p>{ name }</p>
             {/*<BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's blogs" />*/}
         </div>
      );
