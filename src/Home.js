f7b147f56f7e62bc1e7250ec356e037670661c8f
Sebastian Ortiz-Cabello <sebastian.ortiz@jockey.com>
2022-12-02 15:12:03 -0600

    Chapter 18 - Conditional Loading Message & Timeout

--- src/Home.js
+++ src/Home.js
@@ -3,23 +3,27 @@ import BlogList from "./BlogList";
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null)
-
+    const [isPending, setIsPending] = useState(true);
     //const [name, setName] = useState('mario');
 
     //The hook below is used every time the website renders something.
     useEffect(() => {
         //console.log('use effect ran')
-        fetch('http://localhost:8000/blogs')
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
             .then(res => {
                 return res.json();
             })
             .then(data => {
                 setBlogs(data)
+                setIsPending(false)
             })
+        }, 1000)
     }, []);
 
     return ( 
         <div className="home">
+            { isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
 
             {/*<button onClick={() => setName('luigi')}>change name</button>
