74464f65ce457469d9a5a53e2bed0db3a1f793c8
Sebastian Ortiz-Cabello <sebastian.ortiz@jockey.com>
2022-12-02 13:49:08 -0600

    Chapter 16 - Using JSON Server

--- src/Home.js
+++ src/Home.js
@@ -18,14 +18,16 @@ const Home = () => {
     //The hook below is used every time the website renders something.
     useEffect(() => {
         console.log('use effect ran')
-        console.log(name);
-    }, [name]);
+        
+    }, []);
 
     return ( 
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
-            <button onClick={() => setName('luigi')}>change name</button>
-            <p>{ name }</p>
+
+            {/*<button onClick={() => setName('luigi')}>change name</button>
+            <p>{ name }</p>*/}
+            
             {/*<BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's blogs" />*/}
         </div>
      );
