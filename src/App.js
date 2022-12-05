7fbad3d7e6df03ef642bdc522d50cf483d3fa17e
Sebastian Ortiz-Cabello <sebastian.ortiz@jockey.com>
2022-12-05 11:49:09 -0600

    Final Chapter - Project Completed!

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Create from './Create';
 import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 
 function App() {
 
@@ -12,15 +13,23 @@ function App() {
       <Navbar />
       <div className="content">    
         <Switch>
+          
           <Route exact path='/'>
             <Home />
           </Route>
+
           <Route exact path='/create'>
             <Create />
           </Route>
+
           <Route exact path='/blogs/:id'>
             <BlogDetails />
           </Route>
+
+          <Route path={'*'}>
+            <NotFound />
+          </Route>
+
         </Switch>
       </div>
     </div>
